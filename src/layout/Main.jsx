13d274952a36ba436d7aa824453fdb57b895e96f
Suraiya Etu <you@example.com>
2025-04-11 21:50:38 +0600

    fixed navbar issue

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
-import Navbar from "../pages/shared/navbar/Navbar";
+import Navbar from "../pages/Shared/NavBar/NavBar";
 
 
 const Main = () => {
